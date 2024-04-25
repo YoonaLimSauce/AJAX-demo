3489498c5bb14ad212db6adcb8c734062e27ac38
Yoona Lim <miraclefishleong@gmail.com>
2024-04-25 22:34:00 +0800

    使用express模块创建一个简易的服务器，监听8000端口，响应get请求并返回hello world字符串

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+<!--
+ * @Author: Yoona Lim miraclefishleong@gmail.com
+ * @Date: 2024-04-25 22:26:15
+ * @LastEditors: Yoona Lim miraclefishleong@gmail.com
+ * @LastEditTime: 2024-04-25 22:26:28
+ * @FilePath: \AJAX-demo\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 # AJAX-demo
 This is a demo project for AJAX.
---
+++ server.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Yoona Lim miraclefishleong@gmail.com
+ * @Date: 2024-04-25 22:30:52
+ * @LastEditors: Yoona Lim miraclefishleong@gmail.com
+ * @LastEditTime: 2024-04-25 22:31:52
+ * @FilePath: \AJAX-demo\server.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('Hello World!')
+})
+
+app.listen(8000, () => {
+    console.log('Server is running at http://localhost:8000')
+})
