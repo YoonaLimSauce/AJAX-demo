81a184ea5e832a44a5d134cf0463738071e8609a
Yoona Lim <miraclefishleong@gmail.com>
2024-04-26 23:30:46 +0800

    响应键盘事件，手动/xml网络对象挂载json解析

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-25 22:30:52
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-25 22:38:51
+ * @LastEditTime: 2024-04-26 23:27:28
  * @FilePath: \AJAX-demo\server.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,6 +19,18 @@ app.post('/server', (request, response) => {
     response.setHeader('Access-Control-Allow-Origin', '*')
     response.setHeader('Access-Control-Allow-Headers', '*')
     response.send('Hello World post AJAX!')
+})
+
+app.all('/json-server', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Content-Type', 'application/json')
+    // response.send({
+    //     message: 'Hello World get post put and delete JSON AJAX!'
+    // })
+    const message = {
+        name: '张敏洁'
+    }
+    response.send(JSON.stringify(message))
 })
 
 app.listen(8000, () => {
