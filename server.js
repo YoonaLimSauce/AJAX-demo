087ab92ec75e99f86fb1d35ae83db5641ed0eb8f
Yoona Lim <miraclefishleong@gmail.com>
2024-04-27 11:42:04 +0800

    服务器可以设置延迟发送响应，客户端可以设置延迟与网络错误处理

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-25 22:30:52
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-26 23:27:28
+ * @LastEditTime: 2024-04-27 11:40:59
  * @FilePath: \AJAX-demo\server.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,6 +31,13 @@ app.all('/json-server', (request, response) => {
         name: '张敏洁'
     }
     response.send(JSON.stringify(message))
+})
+
+app.get('/delay', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    setTimeout(() => {
+        response.send('延时响应')
+    }, 3000);
 })
 
 app.listen(8000, () => {
