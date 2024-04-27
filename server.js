4681e7ce2dad371a0eb1f8e1e098b36c1b1d321c
Yoona Lim <miraclefishleong@gmail.com>
2024-04-27 23:08:03 +0800

    使用axios进行ajax网络请求

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-25 22:30:52
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-27 18:06:07
+ * @LastEditTime: 2024-04-27 22:57:32
  * @FilePath: \AJAX-demo\server.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 app.get('/server', (request, response) => {
     response.setHeader('Access-Control-Allow-Origin', '*')
@@ -58,6 +60,20 @@ app.all('/ajax-server', (request, response) => {
         gender: '女'
     }
     response.send(JSON.stringify(data))
+})
+
+app.get('/axios-server', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', '*')
+
+    response.send('Welcome to axios AJAX!')
+})
+
+app.post('/axios-server', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', '*')
+
+    response.send('Welcome to axios AJAX!')
 })
 
 app.listen(8000, () => {
